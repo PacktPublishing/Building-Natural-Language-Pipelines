c03e16f9af85422e19cc278bece4d07a9695c6d3
Laura Gutierrez Funderburk <lgutierrwr@gmail.com>
2025-12-16 21:43:59 -0800

    Add Hayhooks server status check and cleanup imports
    Added code cells in both haystack_agent_with_tools.ipynb and haystack_looping_supervisor.ipynb to test the Hayhooks server connection and provide user feedback. Also removed an unused import (YelpAgentState) from haystack_helpers/__init__.py for code cleanliness.

--- ch8/yelp-navigator/haystack_helpers/__init__.py
+++ ch8/yelp-navigator/haystack_helpers/__init__.py
@@ -1,11 +1,9 @@
 """Haystack helpers for multi-agent system."""
 
-from .state import YelpAgentState
 from .components import (ClarificationComponent, StateMultiplexer,\
                                         SearchComponent, DetailsComponent, SentimentComponent, SummaryComponent,\
                                         SupervisorComponent)
 __all__ = [
-    "YelpAgentState",
     "ClarificationComponent",
     "StateMultiplexer",
     "SearchComponent",
